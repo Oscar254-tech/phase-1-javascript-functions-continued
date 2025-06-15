5d4b88d70287861538f1126af6a614248dabff9e
oscar ochanda <oscarochanda@gmail.com>
2025-06-15 21:33:17 +0300

    Functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+const mondayWork = function(task = "go to the office") {
+  return `This Monday, I will ${task}.`;
+};
+function wrapAdjective(flair = "*") {
+  // This is the inner function (a closure) that has access to 'flair' from its outer scope.
+  return function(adjective = "special") { // Inner function's parameter defaults to "special"
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
